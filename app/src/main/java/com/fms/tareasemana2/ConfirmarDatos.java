fa121f3a17fa8987cb8e60081c58ae05686ff1a1
fmsanturio <fmsanturio@hotmail.com>
2020-10-28 10:36:29 -0300

    Initial commit

---
+++ app/src/main/java/com/fms/tareasemana2/ConfirmarDatos.java
@@ -0,0 +1,88 @@
+package com.fms.tareasemana2;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfirmarDatos extends AppCompatActivity {
+
+    // Elementos de la vista
+    TextView tvName;
+    TextView tvBirthdate;
+    TextView tvPhone;
+    TextView tvEmail;
+    TextView tvDescriptionContent;
+    Button btEditData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_datos);
+
+        // Obtenemos el bundle con los extras
+        Bundle b = getIntent().getExtras();
+
+        // Asignamos elementos de la vista
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvBirthdate = (TextView) findViewById(R.id.tvBirthdate);
+        tvPhone = (TextView) findViewById(R.id.tvPhone);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvDescriptionContent = (TextView) findViewById(R.id.tvDescriptionContent);
+        btEditData = (Button) findViewById(R.id.btEditData);
+
+        // Ponemos los extras en sus correspondientes TextView
+        tvName.setText(b.getString("nombre"));
+        tvBirthdate.setText(b.getString("fecha"));
+        tvPhone.setText(b.getString("telefono"));
+        tvEmail.setText(b.getString("email"));
+        tvDescriptionContent.setText(b.getString("descripcion"));
+
+        // Asignamos el listener al botón
+        btEditData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(creaIntent());
+                finish();
+            }
+        });
+    }
+
+    // Se devuelve el Intent con los extras listo para llamar a MainActivity
+    private Intent creaIntent() {
+
+        Intent intent = new Intent(this,MainActivity.class);
+
+        // Obtenemos los datos de la pantalla para introducir como extras al intent
+        String nombre = tvName.getText().toString();
+        if (nombre.equals("No se ha introducido nombre."))
+            nombre = null;
+
+        String fecha = tvBirthdate.getText().toString();
+
+        String telefono = tvPhone.getText().toString();
+        if (telefono.equals("No se ha introducido teléfono."))
+            telefono = null;
+
+        String email = tvEmail.getText().toString();
+        if (email.equals("No se ha introducido dirección de correo."))
+            email = null;
+
+        String descripcion = tvDescriptionContent.getText().toString();
+        if (descripcion.equals("No se ha introducido descripción."))
+            descripcion = null;
+
+        // Los introducimos al intent
+        intent.putExtra("nombre", nombre);
+        intent.putExtra("fecha", fecha);
+        intent.putExtra("telefono", telefono);
+        intent.putExtra("email", email);
+        intent.putExtra("descripcion", descripcion);
+
+        return intent;
+    }
+}
